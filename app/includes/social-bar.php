54a04ab49c12afc15f25120563827fd8a44125c1
Phuc1810 <tranhoaiphuc1810@gmail.com>
2025-12-18 21:47:27 +0700

    Delete app directory

--- app/includes/social-bar.php
+++
@@ -1,8 +0,0 @@
-<div class="thanh_mxh">
-  <a href="#" class="nut_mxh"><i class="fa-solid fa-calendar-days"></i></a>
-  <a href="#" class="nut_mxh"><i class="fa-solid fa-phone"></i></a>
-  <a href="#" class="nut_mxh"><i class="fa-brands fa-facebook-messenger"></i></a>
-  <a href="#" class="nut_mxh"><i class="fa-brands fa-zalo"></i></a>
-  <a href="#" class="nut_mxh"><i class="fa-solid fa-map-location-dot"></i></a>
-  <a href="#" id="ve_dau_trang" class="nut_mxh"><i class="fa-solid fa-arrow-up"></i></a>
-</div>
